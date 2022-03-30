db4738f57eb80831ae31e558e3e652078c0417b6
marzan131390 <marzandwmtec@gmail.com>
2022-03-30 10:51:29 +0600

    input all clear and single value clear is completed

--- js/script.js
+++ js/script.js
@@ -14,5 +14,34 @@ let generateBtn = document.querySelector(".generate-btn");
 generateBtn.addEventListener("click", function() {
     let inputField = document.getElementById("left-input");
     inputField.value = getPin();
-})
+});
+
+//Working with key pad....
+let rightInput = document.getElementById("right-input");
+let keyBody = document.getElementById("key-body");
+keyBody.addEventListener("click", function(event) {
+    let outputValue = event.target.innerText;
+    //Clearing All Input Field if C is clicked..
+    if(isNaN(outputValue)) {
+        if(outputValue == "C") {
+            rightInput.value = " ";
+        }
+    } 
+    //Display Number Value Only...
+    else {
+        let previousValue = rightInput.value;
+        let newValue = previousValue + outputValue;
+        rightInput.value = newValue;
+    }
+});
+//Every Single Value will be removed if backspace is clicked..
+let backSpace = document.getElementById("backspace");
+backSpace.addEventListener("click", function() {
+    let outputValue = rightInput.value;
+    let newValue = outputValue.slice(0, -1);
+    rightInput.value = newValue;
+});
+
+
+
 
